00f5f25ceb8a20e2c41cdba65100d79c8977b315
MrWanted <wanted.lepota@gmail.com>
2022-12-26 06:18:56 +0200

    add the structure of the api, update properties files

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -2,12 +2,23 @@ package com.example.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
-
+	@GetMapping("/ping")
+	public String pong(){
+		return "pong";
+	}
+	@RequestMapping("/")
+	public String home() {
+		return "Welcome to Afrika";
+	}
 }
