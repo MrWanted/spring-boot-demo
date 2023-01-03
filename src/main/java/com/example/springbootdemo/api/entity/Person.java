3031c4f66df069a2c38d95cfef4d7033b1c37b50
MrWanted <wanted.lepota@gmail.com>
2023-01-03 14:36:23 +0200

    person controller

--- src/main/java/com/example/springbootdemo/api/entity/Person.java
+++ src/main/java/com/example/springbootdemo/api/entity/Person.java
@@ -1,17 +1,20 @@
 package com.example.springbootdemo.api.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serializable;
 
-@Entity
 @Data
+@Entity
+//@Table(name = "person", schema = "public")
 public class Person implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
     private Long id;
     private String name;
     private String surname;
+  //  private String email;
 }
--- src/main/java/com/example/springbootdemo/api/rest/controllers/PersonController.java
+++ src/main/java/com/example/springbootdemo/api/rest/controllers/PersonController.java
@@ -4,11 +4,13 @@ import com.example.springbootdemo.api.entity.Person;
 import com.example.springbootdemo.api.service.PersonService;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @RestController
 @RequestMapping("/rest/api/person")
@@ -16,6 +18,15 @@ import java.util.Optional;
 @Data
 public class PersonController {
     private final PersonService service;
+    @Autowired
+    private KafkaTemplate <String, String> kafkaTemplate;
+
+    @GetMapping("/ping")
+    public String pong(){
+        this.kafkaTemplate.send("investor_withdrawals","test topic");
+        System.out.println(this.kafkaTemplate.getDefaultTopic());
+        return "pong";
+    }
 
     @GetMapping("")
     public ResponseEntity<List<Person>> findAll() {
@@ -23,11 +34,22 @@ public class PersonController {
     }
 
     @PostMapping("")
-    public ResponseEntity save(Person person) {
+    public ResponseEntity save(@RequestBody Person person) {
         log.info("Saving person details...");
+
         return new ResponseEntity<>(service.save(person), HttpStatus.OK);
     }
 
+    @PutMapping("/{id}")
+    public Person update(@RequestBody Person newPerson, @PathVariable Integer id) {
+        log.info("update person details...");
+        return service.findByID(id)
+                .map(person -> {
+                    person.setSurname(newPerson.getSurname());
+                    return service.save(person);
+                })
+                .orElseGet(() -> service.save(newPerson));
+    }
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Person>> findById(@PathVariable Integer id) {
         log.info("find investor details by id ...", id);
