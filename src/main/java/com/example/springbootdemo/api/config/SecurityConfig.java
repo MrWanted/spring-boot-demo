3082fc2b3efd73179826715aa903a6be39fd2a1f
MrWanted <wanted.lepota@gmail.com>
2023-01-14 09:06:43 +0200

    update security config

--- src/main/java/com/example/springbootdemo/api/config/SecurityConfig.java
+++ src/main/java/com/example/springbootdemo/api/config/SecurityConfig.java
@@ -2,28 +2,37 @@ package com.example.springbootdemo.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        //Since H2 console runs within a frame so while Spring security is enabled, frame options has to be disabled explicitly, in order to get the H2 console working.
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-
-        return http
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/").authenticated();
-                    auth.anyRequest().permitAll();
-                })
-                .httpBasic(withDefaults())
-                .build();
+    public InMemoryUserDetailsManager user() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build());
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        //Since H2 console runs within a frame so while Spring security is enabled, frame options has to be disabled explicitly, in order to get the H2 console working.
+        httpSecurity.headers().frameOptions().disable();
+
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
 }
