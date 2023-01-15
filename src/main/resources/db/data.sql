1b0e3d349dd89c387a8e55983b22e28461f2527f
MrWanted <wanted.lepota@gmail.com>
2023-01-15 12:48:59 +0200

    update the data and schema scripts

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,1 +1,5 @@
-insert into person (id,name, surname) values (1,'makgape1', 'lepota');
+select next value for banking_details_seq;
+insert into banking_details (account_number, account_type, bank_name, branch_code, bank_id) values (?, ?, ?, ?, ?);
+insert into person (id, fk_bank_id, name, surname) values (default, ?, ?, ?);
+
+
