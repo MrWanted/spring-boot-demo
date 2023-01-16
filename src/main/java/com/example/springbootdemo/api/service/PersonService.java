93fd87a249a000a2530d872846a307ecfffa319d
MrWanted <wanted.lepota@gmail.com>
2023-01-16 22:22:01 +0200

    remove unused import

--- src/main/java/com/example/springbootdemo/api/service/PersonService.java
+++ src/main/java/com/example/springbootdemo/api/service/PersonService.java
@@ -1,9 +1,7 @@
 package com.example.springbootdemo.api.service;
 
 import com.example.springbootdemo.api.entity.Person;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface PersonService {
     Person save(Person product);
