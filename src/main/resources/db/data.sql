3e519b96a05c893baad0ed69e8275b8f242ea9a5
MrWanted <wanted.lepota@gmail.com>
2023-01-18 23:23:23 +0200

    update script to insert data

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,5 +1,9 @@
 select next value for banking_details_seq;
+select next value for department_seq;
 insert into banking_details (account_number, account_type, bank_name, branch_code, bank_id) values (?, ?, ?, ?, ?);
-insert into person (id, fk_bank_id, name, surname) values (default, ?, ?, ?);
-
+insert into department (code, name, id) values (?, ?, ?);
+insert into employee (id, fk_bank_id, fk_department_id, name, surname) values (default, ?, ?, ?, ?);
+select next value for project_seq;
+insert into project (code, name, id) values (?, ?, ?);
+insert into employee_project (employee_id, project_id) values (?, ?);
 
