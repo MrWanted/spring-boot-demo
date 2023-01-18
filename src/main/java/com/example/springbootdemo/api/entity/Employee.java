b1f187e8f05ca4f2de17f52291ce2f50483e2889
MrWanted <wanted.lepota@gmail.com>
2023-01-18 22:21:24 +0200

    add project and department relationships

--- src/main/java/com/example/springbootdemo/api/entity/Employee.java
+++ src/main/java/com/example/springbootdemo/api/entity/Employee.java
@@ -1,22 +1,31 @@
 package com.example.springbootdemo.api.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @Entity
-public class Person implements Serializable {
+public class Employee implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, unique = true)
     private Integer id;
     private String name;
     private String surname;
-
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "fk_bank_id")
     private BankingDetails bankingDetails;
+    @ManyToOne
+    @JoinColumn(name = "fk_department_id")
+    private Department department;
+    @ManyToMany
+    @JoinTable(
+            name = "employee_project",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "project_id")
+    )
+    private List<Project> projects;
 }
