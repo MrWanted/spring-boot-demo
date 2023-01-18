1123c0e7ad14d2fe01ed7b20874674ced176f5b8
MrWanted <wanted.lepota@gmail.com>
2023-01-18 22:26:50 +0200

    rename class name

--- src/main/java/com/example/springbootdemo/api/rest/controllers/EmployeeController.java
+++ src/main/java/com/example/springbootdemo/api/rest/controllers/EmployeeController.java
@@ -1,40 +1,39 @@
 package com.example.springbootdemo.api.rest.controllers;
 
-import com.example.springbootdemo.api.entity.Person;
-import com.example.springbootdemo.api.service.PersonService;
+import com.example.springbootdemo.api.entity.Employee;
+import com.example.springbootdemo.api.service.EmployeeService;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/rest/api/person")
 @Slf4j
 @Data
-public class PersonController {
-    private final PersonService service;
+public class EmployeeController {
+    private final EmployeeService service;
 
     @GetMapping("")
-    public ResponseEntity<List<Person>> findAll() {
+    public ResponseEntity<List<Employee>> findAll() {
         return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
     }
 
     @PostMapping("")
-    public ResponseEntity save(@RequestBody Person person) {
+    public ResponseEntity save(@RequestBody Employee person) {
         log.info("Saving person details...");
         return new ResponseEntity<>(service.save(person), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public Person update(@RequestBody Person person, @PathVariable Integer id) {
+    public Employee update(@RequestBody Employee person, @PathVariable Integer id) {
         log.info("updating person details...");
         return service.update(person, id);
     }
     @GetMapping("/{id}")
-    public ResponseEntity<Person> findById(@PathVariable Integer id) throws Exception {
+    public ResponseEntity<Employee> findById(@PathVariable Integer id) throws Exception {
         log.info("find a person details by id ...", id);
         return new ResponseEntity<>(service.findByID(id), HttpStatus.OK);
     }
