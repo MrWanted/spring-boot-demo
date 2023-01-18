c60b7e55cdfd889283248618091743e8d093fc73
MrWanted <wanted.lepota@gmail.com>
2023-01-18 22:23:36 +0200

    rename

--- src/main/java/com/example/springbootdemo/api/repository/EmployeeRepository.java
+++ src/main/java/com/example/springbootdemo/api/repository/EmployeeRepository.java
@@ -1,10 +1,10 @@
 package com.example.springbootdemo.api.repository;
 
 
-import com.example.springbootdemo.api.entity.Person;
+import com.example.springbootdemo.api.entity.Employee;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends ListCrudRepository<Person, Integer> {
+public interface EmployeeRepository extends ListCrudRepository<Employee, Integer> {
 }
