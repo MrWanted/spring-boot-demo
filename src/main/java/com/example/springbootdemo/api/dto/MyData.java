4718a14f16ad30ecd123c37f1146e9e789080140
MrWanted <wanted.lepota@gmail.com>
2023-03-15 23:45:12 +0200

    add processing functionality for xml

---
+++ src/main/java/com/example/springbootdemo/api/dto/MyData.java
@@ -0,0 +1,25 @@
+package com.example.springbootdemo.api.dto;
+
+public class MyData {
+    private String name;
+    private int age;
+
+    // Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
--- src/main/java/com/example/springbootdemo/api/rest/controllers/HomeController.java
+++ src/main/java/com/example/springbootdemo/api/rest/controllers/HomeController.java
@@ -1,13 +1,18 @@
 package com.example.springbootdemo.api.rest.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springbootdemo.api.dto.MyData;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
     @RequestMapping("/")
     public @ResponseBody String greeting() {
         return "Hello, Afrika";
+    }
+    @PostMapping(value = "/data", consumes = { MediaType.APPLICATION_XML_VALUE , MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<String> handleData(@RequestBody MyData data) {
+        return ResponseEntity.ok("json Data processed successfully");
     }
 }
