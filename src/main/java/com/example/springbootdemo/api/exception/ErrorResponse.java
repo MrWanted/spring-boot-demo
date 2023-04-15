3ef2330946ac1afabb6166b4b5476fc3b8e7ec50
MrWanted <wanted.lepota@gmail.com>
2023-04-15 09:50:47 +0200

    convert class to record

--- src/main/java/com/example/springbootdemo/api/exception/ErrorResponse.java
+++ src/main/java/com/example/springbootdemo/api/exception/ErrorResponse.java
@@ -1,11 +1,3 @@
 package com.example.springbootdemo.api.exception;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
 public record ErrorResponse(int errorCode, String errorMessage) {
 }
